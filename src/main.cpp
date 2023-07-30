462dc39f32cbef479e7ea09eaa8aa282714c84b2
Salmaan Saeed <salsaeed999@gmail.com>
2023-07-30 19:14:15 -0400

    initialized GLEW for the program

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
 #include <iostream>
@@ -9,10 +10,12 @@ int main(void)
 {
     GLFWwindow* window;
 
+    // Initialize GLFW
     if (!glfwInit()) {
         return -1;
     }
 
+    // Create Window (GLFW)
     window = glfwCreateWindow(640, 480, "OepnGL Demo", NULL, NULL);
 
     if (!window) {
@@ -21,6 +24,16 @@ int main(void)
     }
 
     glfwMakeContextCurrent(window);
+
+    // Initialize GLEW
+    GLenum err = glewInit();
+
+    if (err != GLEW_OK) {
+        std::cout << "Error: " << glewGetErrorString(err) << "" << std::endl;
+    }
+    else {
+        std::cout << "Status: Using GLEW " << glewGetString(GLEW_VERSION) << "" << std::endl;
+    }
 
     while (!glfwWindowShouldClose(window)) {
         /* Game loop */
