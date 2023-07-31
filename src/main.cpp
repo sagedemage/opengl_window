321b753ba5b3b924d71da269b2bd024949b6776f
Salmaan Saeed <salsaeed999@gmail.com>
2023-07-31 18:57:02 -0400

    implemented the shader portion in order to render a triangle with a custom color

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,24 @@
 
 //#include "main_window/main_window.h"
 
+const unsigned int SCREEN_WIDTH = 640;
+const unsigned int SCREEN_HEIGHT = 480;
+
+// Shader Source Code (GLSL code)
+const char *vertexShaderSource = "#version 330 core\n"
+    "layout (location = 0) in vec3 aPos;\n"
+    "void main()\n"
+    "{\n"
+    "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+    "}\0";
+
+const char *fragmentShaderSource = "#version 330 core\n"
+    "out vec4 FragColor;\n"
+    "void main()\n"
+    "{\n"
+    "   FragColor = vec4(1.0f, 0.0f, 0.0f, 1.0f);\n"
+    "}\n\0";
+
 int main(void)
 {
     GLFWwindow* window;
@@ -16,7 +34,7 @@ int main(void)
     }
 
     // Create Window (GLFW)
-    window = glfwCreateWindow(640, 480, "OepnGL Demo", NULL, NULL);
+    window = glfwCreateWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "OepnGL Demo", NULL, NULL);
 
     if (!window) {
         glfwTerminate();
@@ -25,7 +43,7 @@ int main(void)
 
     glfwMakeContextCurrent(window);
 
-    // Initialize GLEW
+    // Initialize GLEW which loads all OpenGL functions
     GLenum err = glewInit();
 
     if (err != GLEW_OK) {
@@ -35,6 +53,73 @@ int main(void)
         std::cout << "Status: Using GLEW " << glewGetString(GLEW_VERSION) << "" << std::endl;
     }
 
+    /* Build and compile the Shader */
+    // vertex shader
+    unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+
+    int success;
+    char infoLog[512];
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+
+    if (!success) {
+        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+        std::cout << "Error: Vertex Shader Compilation Failed" << infoLog << std::endl;
+    }
+
+    // frament shader
+    unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+
+    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+    if (!success) {
+        glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+        std::cout << "Error: Fragment Shader Compilation Failed" << infoLog << std::endl;
+    }
+
+    // link shaders
+    unsigned int shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+
+    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+    if (!success) {
+        glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+        std::cout << "Error: Program Shader Compilation Failed" << infoLog << std::endl;
+    }
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+    /*
+     * setup vertex data and buffers
+     * configure vertex attributes
+     */
+
+    float vertices[] = {
+        -0.5f, -0.5f, 0.0f, // left
+        0.5f, -0.5f, 0.0f, // right
+        0.0f, 0.5f, 0.0f // top
+    };
+
+    unsigned int VBO, VAO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+    glBindVertexArray(0);
+
     while (!glfwWindowShouldClose(window)) {
         /* Game loop */
 
@@ -42,11 +127,17 @@ int main(void)
         glClear(GL_COLOR_BUFFER_BIT);
 
         // Draw triangle to test OpenGL works
+        glUseProgram(shaderProgram);
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+
+        /*
         glBegin(GL_TRIANGLES);
         glVertex2f(-0.5f, -0.5f);
         glVertex2f(0.0f, 0.5f);
         glVertex2f(0.5f, -0.5f);
         glEnd();
+        */
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
@@ -55,6 +146,12 @@ int main(void)
         glfwPollEvents();
     }
 
+    // Dealocate all resources once they have outlived their purpose
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteProgram(shaderProgram);
+
+    // Terminates the GLFW library
     glfwTerminate();
 
     return 0;
